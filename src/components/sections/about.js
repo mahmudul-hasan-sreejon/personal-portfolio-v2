36720592f3ca2d42529c7df64c0be230dfe59018
mahmudul-hasan-sreejon <mahmudul.hasan.sreejon@gmail.com>
2022-04-10 20:26:12 +0600

    Update profile photo

--- src/components/sections/about.js
+++ src/components/sections/about.js
@@ -116,7 +116,7 @@ const StyledPic = styled.div`
 const About = () => {
 	const data = useStaticQuery(graphql`
 		query {
-			avatar: file(sourceInstanceName: { eq: "images" }, relativePath: { eq: "me.jpg" }) {
+			avatar: file(sourceInstanceName: { eq: "images" }, relativePath: { eq: "profile-photo.png" }) {
 				childImageSharp {
 					fluid(maxWidth: 500, traceSVG: { color: "#DC3545" }) {
 						...GatsbyImageSharpFluid_withWebp_tracedSVG
